c4ed2163dbb35aeaae974571cace23b0fc74f930
Christine <huangc909@gmail.com>
2020-11-17 16:02:36 -0500

    Created lifeCategory Schema

---
+++ app/models/lifeCategory.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const lifeCategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  hours: {
+    type: Number,
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('lifeCategory', lifeCategorySchema)
